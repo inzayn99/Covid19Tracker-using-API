420c9a466ffe560244810db276eb2fd0df0a9816
Firoj <mymindhungry@gmail.com>
2021-09-21 14:59:53 +0545

    Real API covid19 traker

--- src/components/covid.js
+++ src/components/covid.js
@@ -1,11 +1,17 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
+import "./covid.css"
+
 
 const Covid = () => {
+
+    const [data, setData] = useState([]);
+
     const getCovidData = async () => {
         try{
             const res = await fetch ('https://data.covid19india.org/data.json');
            const actualData = await res.json();
            console.log(actualData.statewise[0]);
+           setData(actualData.statewise[0]);
         }catch (err) {
             console.log(err);
         }
@@ -18,8 +24,73 @@ const Covid = () => {
     return(
 
 <>
-<h1>LIVE</h1>
-    <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+<section className="cards">
+    <h2 className="heading">LIVE COVID-19 CORONAVIRUS TRACKER</h2>
+   
+
+<ul>
+<li className="card">
+<div className="card__main">
+<div className="card__inner">
+<p className="card__name"><span>OUR</span>COUNTRY</p>
+<p className="card__total card__small">INDIA</p>
+</div>
+</div>
+</li>
+
+
+<li className="card">
+<div className="card__main">
+<div className="card__inner">
+<p className="card__name"><span>TOTAL</span> RECOVERED</p>
+<p className="card__total card__small">{data.recovered}</p>
+</div>
+</div>
+</li>
+
+<li className="card">
+<div className="card__main">
+<div className="card__inner">
+<p className="card__name"><span>TOTAL</span>CONFIRMED</p>
+<p className="card__total card__small">{data.confirmed}</p>
+</div>
+</div>
+</li>
+
+<li className="card">
+<div className="card__main">
+<div className="card__inner">
+<p className="card__name"><span>TOTAL</span>DEATH</p>
+<p className="card__total card__small">{data.deaths}</p>
+</div>
+</div>
+</li>
+
+
+<li className="card">
+<div className="card__main">
+<div className="card__inner">
+<p className="card__name"><span>TOTAL</span>ACTIVE</p>
+<p className="card__total card__small">{data.active}</p>
+</div>
+</div>
+</li>
+
+
+<li className="card">
+<div className="card__main">
+<div className="card__inner">
+<p className="card__name"><span>LAST</span>UPDATED</p>
+<p className="card__total card__small">{data.lastupdatedtime}</p>
+</div>
+</div>
+</li>
+
+
+
+</ul>
+</section>
+
     </>
   )
 }
